264755271b65bf812ed4df2c38e3b911cff3532d
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 08:59:49 +0100

    Add copyright and license to genre_definitions.py
    Added copyright notice and license information.

--- genre_definitions.py
+++ genre_definitions.py
@@ -1,5 +1,10 @@
 # genre_definitions.py
 # -*- coding: utf-8 -*-
+# Copyright (C) 2025 Maciek (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 """
 Definicje Gatunków Muzycznych EriAmo v5.9
 
