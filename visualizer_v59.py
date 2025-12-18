bd47f8f09a4caf67c01a8a0cd869f4edeed694ce
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 08:57:52 +0100

    Add copyright and license information
    Added copyright notice and licensing information.

--- visualizer_v59.py
+++ visualizer_v59.py
@@ -1,6 +1,10 @@
 # visualizer_v59.py
 # -*- coding: utf-8 -*-
-
+# Copyright (C) 2025 Maciek (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
