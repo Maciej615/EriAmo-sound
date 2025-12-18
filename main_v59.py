55b9369e68fc969ab43698a7ecc73232faf73e2d
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 08:59:14 +0100

    Add copyright and license details to main_v59.py
    Added copyright notice and license information.

--- main_v59.py
+++ main_v59.py
@@ -1,5 +1,10 @@
 # main_v59.py
 # -*- coding: utf-8 -*-
+# Copyright (C) 2025 Maciek (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 """
 EriAmo v5.9 - Główny Interfejs CLI (UPDATE)
 - Wsparcie dla wyboru instrumentów przy kompozycji
